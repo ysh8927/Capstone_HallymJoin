861afa8368b818d2e37b54fd8c08026a69170aa5
ysh8927 <ysh8927@naver.com>
2026-04-18 19:06:57 +0900

    fix: prisma.config.ts DATABASE_URL로 수정

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'prisma/config';
-import 'dotenv/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DIRECT_URL!,
+    url: process.env.DATABASE_URL!,
   },
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
